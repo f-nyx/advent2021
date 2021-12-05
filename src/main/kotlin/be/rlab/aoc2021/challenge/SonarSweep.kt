140b293bf9828f0a37170d0a368e4acf85179e5e
f-nyx <dnyx@riseup.net>
2021-12-05 14:46:14 -0300

    Day 1: adds solution documentation

--- src/main/kotlin/be/rlab/aoc2021/challenge/SonarSweep.kt
+++ src/main/kotlin/be/rlab/aoc2021/challenge/SonarSweep.kt
@@ -2,7 +2,23 @@ package be.rlab.aoc2021.challenge
 
 import be.rlab.aoc2021.support.ResourceUtils.loadInput
 
-const val INPUT_NAME: String = "01-sonar_sweep_input.txt"
+/** Day 1: Sonar Sweep
+ *
+ * This challenge is about counting _increments_ in a list of numbers. But instead of making it
+ * easy, it requires counting increments using a _window_. Let's say we have a window of three numbers:
+ *
+ * Step 1: [1 2 3] 4 5 6 = 6
+ * Step 2: 1 [2 3 4] 5 6 = 9
+ * Step 3: 1 2 [3 4 5] 6 = 12
+ * Step 4: 1 2 3 [4 5 6] = 15
+ * Step 5: 1 2 3 4 [5 6] = 11
+ *
+ * The window value is the sum of all numbers in the window. In this example, the window value
+ * increases from steps 1 to 4, so we have 4 _increments_.
+ *
+ * The challenge has two parts, the first part is counting increments with a window of one, and the
+ * second part is counting increments with a window of three.
+ */
 
 fun countIncrements(depths: List<Int>): Int {
     return depths.foldIndexed(0) { index, increments, depth ->
@@ -33,10 +49,9 @@ fun calculateDepthsInWindow(
 }
 
 fun main() {
-    val depths: List<Int> = loadInput(INPUT_NAME).split("\n").map(String::toInt)
+    val depths: List<Int> = loadInput("01-sonar_sweep_input.txt").split("\n").map(String::toInt)
     val depthIncrements: Int = countIncrements(depths)
-    val depthsInWindow: List<Int> = calculateDepthsInWindow(depths, 3)
-    val depthIncrementsInWindow: Int = countIncrements(depthsInWindow)
+    val depthIncrementsInWindow: Int = countIncrements(calculateDepthsInWindow(depths, 3))
     println("Depth increments: $depthIncrements")
     println("Depth increments in window: $depthIncrementsInWindow")
 }
